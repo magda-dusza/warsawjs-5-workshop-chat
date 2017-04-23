e6e8559eba5a68c3b8e30c887ee85261e48ea16c
Magda <magdalena.dusza13@gmail.com>
2017-04-23 16:08:05 +0200

    register / login / logout User

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const io = require('socket.io-client');
-const client = io('http://localhost:3000');
+const client = io(`http://${process.argv[2] || 'localhost'}:3000`);
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -21,10 +21,52 @@ client.on('message', (msg)=>{
 // display '>' sign
 readline.prompt();
 
+let LOGGED_USER = '';
+
 readline.on('line', (line)=>{
-    if(line.trim()){
+    let lineArgs = line.split(/\s+/);
+    let firstWord = lineArgs[0];
+    if(firstWord === '/exit'){
+        readline.close();
+        process.exit();
+    } else if (firstWord === '/register') {
+        if(lineArgs.length === 3) {
+            client.emit('register', {username: lineArgs[1], password: lineArgs[2]});
+        }
+    } else if(firstWord === '/login') {
+        if(lineArgs.length === 3) {
+            client.emit('login', {username: lineArgs[1], password: lineArgs[2]} );
+        } else {
+            client.emit('login', false );
+        }
+    } else if(firstWord === '/logout') {
+        readline.setPrompt('> ');
+        client.emit('logout', LOGGED_USER);
+    }
+     else if(line.trim()){
         client.emit('message', line);
     }
     //new line with '>'
+    readline.prompt();
+});
+
+client.on('login', (username) =>{
+    console.log(username);
+    if(username){
+        readline.setPrompt(`${username} :`)
+        LOGGED_USER = username;
+    } else {
+        console.log('>> LOGIN FAILED!!!');
+    }
+    readline.prompt();
+})
+
+client.on('register', (registerStatus) => {
+    clearPrompt();
+    if(registerStatus){
+        console.log('>> register success');
+    } else {
+        console.log('>> register failed');
+    }
     readline.prompt();
 });
--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 const io = require('socket.io');
 const server = io();
 
+const USERS = {};
+
 server.on('connection', (client) => {
+    let clientName = '';
     console.log(`client with id: ${client.id}, connected`);
     client.on('message', (msg)=>{
         console.log(msg);
         //broadcast send to all clients, despite client who send message
         client.broadcast.emit('message', msg);
+    });
+    client.on('register', (user) => {
+        console.log('register', user);
+        if(USERS[user.username]){
+            client.emit('register', false);
+        }else {
+            USERS[user.username] = {
+                password: user.password,
+                logged_in: false
+            };
+            client.emit('register', true);
+        }
+    });
+    client.on('login', (userObject) => {
+        console.log('login', userObject);
+        const user = USERS[userObject.username];
+        if(!user){
+            console.log('USER DOES NOT EXIST!!!');
+            client.emit('login', false);
+            return;
+        }
+        if(user.password === userObject.password){
+            user.logged_in = true;
+            client.emit('login', userObject.username);
+        } else {
+            client.emit('login', false);
+        }
+    })
+    client.on('logout', (username) => {
+        USERS[username].logged_in = false;
     });
 });
 
